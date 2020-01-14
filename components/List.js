a70ef6de7d87b9d2866d234aa2d1e2d19b6b3878
Kashif Malik <kashifqamar111@gmail.com>
2020-01-14 16:20:44 +0200

    week1Exercise

---
+++ components/List.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, FlatList } from 'react-native';
+import ListItem from './ListItem';
+
+const List = (props) => {
+    return (
+        <View style={{marginTop: 36}}>
+            <FlatList
+                data={props.mediaArray}
+                renderItem= {({item}) => {
+                    return (
+                        <ListItem item={item} />
+                    );
+                }}
+            />
+        </View>
+    );
+};
+
+export default List;
